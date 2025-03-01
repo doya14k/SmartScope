f4c1504eca6def4e67d4f92eab6f8afb19083f0b
dominic_knoepfel <dominicknoepfel@outlook.com>
2025-03-01 16:42:45 +0100

    usb select page integrated

--- smart_scope/lib/pages/usb_select.dart
+++ smart_scope/lib/pages/usb_select.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:serial_port_win32/serial_port_win32.dart';
+// import 'package:flutter_libserialport/flutter_libserialport.dart';
 import 'home_page.dart';
 import 'dart:async';
+import 'settings_pages/settings_widgets/definitions.dart';
 
 class USB_Select extends StatefulWidget {
   const USB_Select({super.key});
@@ -11,66 +13,153 @@ class USB_Select extends StatefulWidget {
 }
 
 class _USB_SelectState extends State<USB_Select> {
-  List<String> ports = [];
+  List<String> availablePorts = [];
   Timer? updatePortsTimer;
 
   @override
   void initState() {
     super.initState();
-<<<<<<< HEAD
-    updatePorts(); 
-=======
     updatePorts();
->>>>>>> 9dd424e012c077db984a4fe4b47cfe847f48d187
   }
 
   void updatePorts() {
     updatePortsTimer = Timer.periodic(Duration(milliseconds: 250), (timer) {
       setState(() {
-        ports = SerialPort.getAvailablePorts();
-        print("Aktualisierte Ports: $ports");
+        availablePorts = SerialPort.getAvailablePorts();
+        // availablePorts = SerialPort.availablePorts;
+        print("Aktualisierte Ports: $availablePorts");
       });
     });
   }
 
+  void switchToMonitorPage() {
+    updatePortsTimer?.cancel();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HomePage()),
+    );
+  }
+
+  void pressedPortSelector(currentPort) {
+    print('${currentPort} selected');
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        toolbarHeight: 120,
+        centerTitle: false,
+        title: Text(
+          'SmartScope',
+          style: TextStyle(
+            fontFamily: 'PrimaryFont',
+            fontWeight: FontWeight.bold,
+            fontSize: 80,
+          ),
+        ),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           SizedBox(
-            height: 100,
-            width: 100,
-            child: FloatingActionButton(
-              child: Text("Switch"),
-              onPressed: () {
-                updatePortsTimer?.cancel();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                );
-              },
-            ),
-          ),
-          Center(
+            width: 400,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text(
-                  'Available Ports:',
-                  style: TextStyle(fontFamily: 'PrimaryFont', fontSize: 40.0),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      '${ports.join("\n")}',
-                      style: TextStyle(
-                        fontFamily: 'PrimaryFont',
-                        fontSize: 40.0,
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        "Available Ports",
+                        style: TextStyle(
+                          fontFamily: 'PrimaryFont',
+                          fontSize: 50,
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 500,
+                        width: 400,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: AppBarBackroundColor,
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(
+                              color: MonitorBackroundColor,
+                              width: 2,
+                            ),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children:
+                                availablePorts
+                                    .map(
+                                      (currentPort) => Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Expanded(
+                                            child: GestureDetector(
+                                              behavior: HitTestBehavior.opaque,
+                                              onDoubleTap: () {
+                                                switchToMonitorPage();
+                                              },
+                                              child: TextButton(
+                                                onPressed: () {
+                                                  pressedPortSelector(
+                                                    currentPort,
+                                                  );
+                                                },
+                                                child: Text(
+                                                  '${currentPort}',
+                                                  style: TextStyle(
+                                                    fontSize: 40,
+                                                    fontFamily: 'PrimaryFont',
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                    .toList(),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            height: 30,
+                            width: 100,
+                            child: FloatingActionButton(
+                              child: Text(
+                                "Apply",
+                                style: TextStyle(
+                                  fontFamily: 'PrimaryFont',
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.italic,
+                                  letterSpacing: 1,
+                                ),
+                              ),
+                              onPressed: () {
+                                switchToMonitorPage();
+                              },
+                              backgroundColor: Colors.grey[400],
+                              hoverColor: Colors.grey[450],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
